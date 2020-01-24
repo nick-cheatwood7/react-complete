19972649b4de438edf6b6fafec2af6e3e1e6f1a3
nick_cheatwood <nicolas@softwarethatfits.com>
2020-01-24 11:34:32 -0600

    End of Section 3

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,15 @@
 import React from "react";
+import "./Person.css";
 
 // functional-based components are intended to inheret props, not handle state
 const person = props => {
   return (
-    <div>
-      <p>
+    <div className="Person">
+      <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 };
